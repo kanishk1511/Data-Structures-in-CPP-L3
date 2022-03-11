0a70c70b4e199c3b4b1e9c90b5933c933faf9ab3
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 15:14:42 +0530

    Create Code: Print Elements in Range.c

---
+++ Lecture 13 : BST/Code: Print Elements in Range.c
@@ -0,0 +1,18 @@
+
+void elementsInRangeK1K2(BinaryTreeNode<int>* root, int k1, int k2) {
+	// Write your code here
+    if (root == NULL)  
+        return;  
+      
+    if(root->data > k1){
+        elementsInRangeK1K2(root->left,k1,k2);
+    }
+    
+    if ( k1 <= root->data && k2 >= root->data )  
+         cout<<root->data<<" ";  
+    
+    if(root->data < k2){
+        elementsInRangeK1K2(root->right,k1,k2);
+    }
+    
+}
