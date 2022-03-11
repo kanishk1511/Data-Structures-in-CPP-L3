9600560aae6f9c5487503a80602187c71c9f754a
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 15:10:30 +0530

    Create check balanced.c

---
+++ Lecture 12 : Binary Trees/check balanced.c
@@ -0,0 +1,30 @@
+#include <algorithm>
+int height (BinaryTreeNode<int> *root){
+    if(root==NULL){
+        return 0;
+    }
+    return 1 + max(height(root->left), height(root->right));
+}
+
+bool isBalanced(BinaryTreeNode<int> *root) {
+	// Write your code here
+    if (root==NULL){
+        return true;
+    }
+    
+    int lh = height(root -> left);
+    int rh = height(root -> right);
+
+    if (lh-rh>1 && rh-lh>1){
+        return false;
+    }
+    
+    if(abs(lh-rh)<=1 ){
+        if(isBalanced (root->left) && isBalanced (root->right))
+            return true;      
+    }
+    
+    return false;
+    
+    
+}
