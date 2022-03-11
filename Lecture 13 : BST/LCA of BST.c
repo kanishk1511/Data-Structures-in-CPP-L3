e86b4a0de6a6312091e55b103cf4f8d0dce9018e
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 15:19:51 +0530

    Create LCA of BST.c

---
+++ Lecture 13 : BST/LCA of BST.c
@@ -0,0 +1,28 @@
+
+int getLCA(BinaryTreeNode<int>* root , int a , int b){
+    // Write your code here
+    if(root==NULL){
+        return -1;
+    }
+    if(root->data==a || root->data==b){
+        return root->data;
+    }
+    else{
+        int left = getLCA(root->left,a,b);
+        int right = getLCA(root->right,a,b);
+        
+        if(left==-1 && right==-1 ){
+            return -1;
+        }
+        else if(left!=-1 && right==-1){
+            return left;
+        }
+        else if(right!=-1 && left==-1){
+            return right;
+        }
+        else if(left!=-1 && right!=-1){
+            return root->data;
+        }
+        
+    }
+}
