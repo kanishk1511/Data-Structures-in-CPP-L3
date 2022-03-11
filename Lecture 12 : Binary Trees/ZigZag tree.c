0a064c8b6fba3f0f41a9f9d7770f188fe930f701
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 15:12:49 +0530

    Create ZigZag tree.c

---
+++ Lecture 12 : Binary Trees/ZigZag tree.c
@@ -0,0 +1,44 @@
+#include<stack>
+
+void zigZagOrder(BinaryTreeNode<int> *root) {
+    // Write your code here
+    if(root==NULL)
+     return;
+    
+    stack<BinaryTreeNode<int> *> s1;
+    stack<BinaryTreeNode<int> *> s2;
+    
+    s1.push(root);
+    
+    while(!s1.empty())
+    {
+        while(!s1.empty())
+        { 
+            BinaryTreeNode<int> * top=s1.top();
+            s1.pop();
+            cout<<top->data<<" ";
+        
+            if(top->left)
+               s2.push(top->left);
+            if(top->right)
+               s2.push(top->right);
+        }
+        
+        cout<<endl;
+        
+        while(!s2.empty())
+        {
+            BinaryTreeNode<int> * top=s2.top();
+            s2.pop();
+            cout<<top->data<<" ";
+            
+            if(top->right)
+               s1.push(top->right);
+            if(top->left)
+               s1.push(top->left);
+       
+        }
+        cout<<endl;
+        
+    }
+}
